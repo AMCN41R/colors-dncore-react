78bb7069ea0028b539e1d8806ec1f84ab9331ba3
Alex McNair <alex.mcnair@hotmail.co.uk>
2018-01-17 23:04:23 +0000

    added basic spa routing

--- ColorsTest/app/index.tsx
+++ ColorsTest/app/index.tsx
@@ -1,18 +1,56 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {
+    HashRouter,
+    Route,
+    Switch,
+    Redirect,
+    withRouter,
+    RouteComponentProps
+} from "react-router-dom";
 
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
 
-
-
-const App = () => (
-    <div className="container">
-        <h2 className="bg-gray">Welcome!</h2>
+const Colors = () => (
+    <div>
+        <h3>Colors</h3>
     </div>
 )
 
-ReactDOM.render(
-    <App />,
+const People = () => (
+    <div>
+        <h3>People</h3>
+    </div>
+)
+
+const Person = ({ match }: RouteComponentProps<any>) => (
+    <div>
+        <h3>Person {match.params.id}</h3>
+    </div>
+)
+
+const NoMatch = ({ location }: RouteComponentProps<any>) => (
+    <div>
+        <h3>No match for <code>{location.pathname}</code></h3>
+    </div>
+)
+
+const App = () => (
+    <div>
+        <Switch>
+            {/* <Redirect from="/" to="/people" /> */}
+            <Route path="/colors" component={Colors} />
+            <Route path="/people/:id" component={Person} />
+            <Route path="/people" component={People} />
+            <Route component={NoMatch} />
+        </Switch>
+    </div>
+)
+
+ReactDOM.render((
+    <HashRouter>
+        <App />
+    </HashRouter>),
     document.getElementById("app")
 );
